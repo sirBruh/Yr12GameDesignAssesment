18703c1104abf53c40aa7f386934489c1a7c3a80
sirBruh <finsta32@kingshigh.school.nz>
2025-06-04 15:00:23 +1200

    Parkour
    Trying a different movement and parkour system, with a bit of patchwork from another video.

--- untitled-delivery-game/Scenes/player.gd
+++ untitled-delivery-game/Scenes/player.gd
@@ -1,72 +1,76 @@
 extends CharacterBody3D
 
-var speed
-const walk_speed = 5.0
-const sprint_speed = 8.0
-const jump_velocity = 4.5
-const sensitivity = 0.003
-@onready var head = $Head
-@onready var camera = $Head/Camera3D
-#FOV
-const base_fov = 75.0
-const fov_change = 1.5
-#bobbing
-const bob_freq = 2.0
-const bob_amp = 0.08
-var t_bob = 0.0
+@onready var stand_col = get_node("standing")
+@onready var crouch_col = get_node("crouching")
+@onready var cam_pivot = get_node("cam_pivot")
+
+@onready var top_head = get_node("raycast/top_head")
+@onready var face_lvl = get_node("raycast/face")
+@onready var new_pos = get_node("raycast/new_pos")
+
+var speed = 5.0
+var jump_velocity = 4.5
+var mouse_sens = 0.002
+var grav = 9.8
+var is_crouching = false
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	 
-func _headbob(time) -> Vector3:
-	var pos = Vector3.ZERO
-	pos.y = sin(time * bob_freq) * bob_amp
-	pos.x = sin(time * bob_freq / 2) * bob_amp
-	return pos
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * sensitivity)
-		camera.rotate_x(-event.relative.y * sensitivity)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-50), deg_to_rad(70))
-
-
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	control_loop(delta)
+	run_state()
+	check_mantle()
+	head_control()
+	move_and_slide()
+
+func control_loop(delta):
+		# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	#Jumping
+	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = jump_velocity
-	#Sprinting
-	if Input.is_action_pressed("Sprint"):
-		speed = sprint_speed
-	else:
-		speed = walk_speed
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
-			velocity.x = direction.x * speed
-			velocity.z = direction.z * speed
+			if is_crouching:
+				velocity.x = direction.x * speed / 2
+				velocity.z = direction.z * speed / 2
+			else:
+				velocity.x = direction.x * speed
+				velocity.z = direction.z * speed 
 		else:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 10)
+			velocity.z = lerp(velocity.z, direction.x * speed, delta * 10)
 	else:
-		velocity.x = lerp(velocity.x, direction.x * speed, delta * 2.0)
-		velocity.z = lerp(velocity.z, direction.z * speed, delta * 2.0)
-	#bobbing
-	t_bob += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = _headbob(t_bob)
-	
-	#FOV
-	var velocity_clamped = clamp(velocity.length(), 0.5, sprint_speed * 2)
-	var target_fov = base_fov + fov_change * velocity_clamped
-	camera.fov = lerp(camera.fov, target_fov, delta * 13.0)
-	
-	move_and_slide()
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 2)
+		velocity.z = lerp(velocity.z, direction.x * speed, delta * 2)
+func head_control():
+	if Input.is_action_just_pressed("Unlock"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE else Input.MOUSE_MODE_VISIBLE
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sens)
+		cam_pivot.rotate_x(-event.relative.y * mouse_sens)
+		cam_pivot.rotation.x = clamp(cam_pivot.rotation.x, -PI/2, PI/2)
+	if event.is_action_pressed("Crouch"):
+		check_crouch_state()
+		is_crouching = not is_crouching
+func check_crouch_state():
+	stand_col.disabled = not is_crouching
+	crouch_col.disabled = is_crouching
+func run_state():
+	if Input.is_action_pressed("Sprint"):
+		speed = 8
+	else:
+		speed = 5
+func check_mantle():
+	var has_ledge = (face_lvl.is_colliding() and not top_head.is_colliding())
+	if has_ledge:
+		velocity.y = 0
