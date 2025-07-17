4480bb3ca2b900ca352e0823790e07d1534a581a
sirBruh <finsta32@kingshigh.school.nz>
2025-07-17 16:23:08 +1200

    trial and error

--- untitled-delivery-game/Scenes/options.gd
+++ untitled-delivery-game/Scenes/options.gd
@@ -1,5 +1,5 @@
 extends Control
-var new_resolution = Vector2i(1152, 648)
+var new_resolution = get_viewport()
 func _ready() -> void:
 	var resolution = $MenuButton
 	var popup = resolution.get_popup()
@@ -9,6 +9,14 @@ func _ready() -> void:
 	popup.add_item("640 x 480", 4)
 func _on_item_pressed(id):
 	if id == 1:
-		new_resolution = Vector2i(1920,1080)
+		new_resolution = Vector2(1920,1080)
+		print("1920x1080")
+	if id == 2:
+		new_resolution = Vector2(1600,900)
+	if id == 3:
+		new_resolution = Vector2(1280,720)
+	if id == 4:
+		new_resolution = Vector2(640,480)
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	new_resolution = Vector2(1920,1080)
